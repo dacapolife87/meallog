be7311d392c4555e03b4e58aca0e816446c5e2bc
HyungjuJang <dacapolife87@gmail.com>
2016-01-29 00:43:53 +0900

    meallog_main
    meallog 첫화면

--- src/main/java/meallog/common/controller/mainController.java
+++ src/main/java/meallog/common/controller/mainController.java
@@ -1,5 +1,22 @@
 package meallog.common.controller;
 
-public class mainController {
+import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import meallog.meal.service.MealService;
+import meallog.meal.vo.Meal;
+
+public class mainController {
+	@Resource(name="meainService")
+    private MainService mainService;
+     
+    @RequestMapping(value="/main.do")
+    public ModelAndView introMeallog() throws Exception{
+        ModelAndView mv = new ModelAndView("/home/meallogin.html");
+        return mv;
+    }
 }
